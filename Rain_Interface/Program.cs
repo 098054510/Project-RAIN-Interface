9c563aa865196d3a172e080a3b6cba88becba2ec
Crisco <eduardohsfreitas86@gmail.com>
2019-04-28 20:50:29 -0300

    Creation of main menu

---
+++ Rain_Interface/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Rain_Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            int option = 0;
+            while (option == 0)
+            {
+                try
+                {
+                    Console.Clear();
+                    Screen.Main_Screen();
+                    option = int.Parse(Console.ReadLine());
+                    switch (option)
+                    {
+                        case 1:
+                            Screen.Functions();
+                            try
+                            {
+                                option = int.Parse(Console.ReadLine());
+                            }
+
+                            catch (Exception e)
+                            {
+                                Console.WriteLine("Internal Error " + e.Message);
+                                option = 0;
+                            }
+                            break;
+                        case 2:
+                            Screen.Options();
+                            try
+                            {
+                                option = int.Parse(Console.ReadLine());
+                            }
+                            catch (Exception e)
+                            {
+                                Console.WriteLine("Internal Error " + e.Message);
+                                option = 0;
+                            }
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Internal Error " + e.Message);
+                    option = 0;
+                }
+                break;
+            }
+        }
+    }
+}
---
+++ Rain_Interface/Screen.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rain_Interface
+{
+    class Screen
+    {
+        public static void Main_Screen()
+        {
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("--------Welcome To RAIN--------");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("Started at: " + DateTime.Now);
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("Type what you want do");
+            Console.WriteLine("1 - Functions");
+            Console.WriteLine("2 - Options");
+            Console.WriteLine("3 - Exit");
+            Console.Write("Your Choice: ");
+        }
+
+        public static void Functions()
+        {
+            Console.WriteLine("----Functions----");
+            Console.WriteLine("1 - Send Email");
+            Console.WriteLine("2 - Calculator");
+            Console.WriteLine("3 - Stopwatch");
+            Console.WriteLine("4 - Music Player");
+            Console.WriteLine("5 - Back to Main Menu");
+            Console.Write("Your Choice: ");
+        }
+
+        public static void Options()
+        {
+            Console.WriteLine("----Options----");
+            Console.WriteLine("1 - Language");
+            Console.WriteLine("2 - Set Time");
+            Console.Write("Your Choice: ");
+        }
+    }
+}
